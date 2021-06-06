d95dffa0f63729e2870a58bbe298818fc695def4
Santhiya <santhiyaps@gmail.com>
2021-06-06 14:47:02 -0700

    June Leetcode

---
+++ src/main/java/week1/LongestConsecutive.java
@@ -0,0 +1,30 @@
+package week1;
+
+import java.util.Arrays;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        if(nums==null || nums.length==0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count=1;
+        int maxCount=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]!=nums[i-1]){
+                if(nums[i]-nums[i-1]==1) {
+                    count++;
+                    maxCount=Math.max(count,maxCount);
+                }
+                else{
+                    count=1;
+                }
+            }
+        }
+        return maxCount;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestConsecutive().longestConsecutive(new int[]{0,3,7}));
+    }
+}
